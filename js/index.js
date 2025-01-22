367d708fa65b85fb88b8d60f45913458731547bd
Drama <dramamailnet@gmail.com>
2025-01-22 23:22:05 +0300

    Project is ready

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const sequelize = require('./util/database')
+const User = require('./models/users')
+
+const app = express()
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Docker World!!!')
+})
+
+
+app.post('users', async (req, res) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email
+    })
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(5000).json(error)
+  }
+})
+
+
+sequelize.sync({force: false})
+  .then(() => app.listen(process.env.EXTERNALPORT))
+  .catch(err => console.log(err))
+
+//const port = 3000 
+//app.listen(port, () => {
+//  console.log(`Example app listening at http://localhost:${port}`)
+//})
---
+++ python/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"postgresql://{os.environ['PG_USER']}"
+    f":{os.environ['PG_PASSWORD']}"
+    f"@{os.environ['PG_HOST']}"
+    f":{os.environ['PG_PORT']}"
+    f"/{os.environ['PG_DATABASE']}"
+)
+
+
+db = SQLAlchemy(app)
+
+
+# Model
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False) 
+    content = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+db.create_all()
+
+@app.route('/', methods=['GET'])
+def get():
+    return 'ok'
+
+
+@app.route('/items', methods=['POST'])
+def itemadd():
+    request_data = request.get_json()
+
+    title = request_data['title']
+    content = request_data['content']
+
+    db.session.add(Item(title, content))
+    db.session.commit()
+
+    return "item created"
